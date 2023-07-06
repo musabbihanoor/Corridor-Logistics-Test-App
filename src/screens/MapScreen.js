7524d85d02c1a4fd6a433950c71b282b3d25ed03
musabbihanoor <musabbihanoor33@gmail.com>
2023-07-07 01:20:39 +0500

    implemented google map

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,12 +1,53 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-const MapScreen = () => {
+import { connect } from "react-redux";
+import { fetchVenues } from "../actions/venueActions";
+
+const MapScreen = ({ venues, loading, error, fetchVenues }) => {
+  useEffect(() => {
+    fetchVenues();
+  }, [fetchVenues]);
+
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 25.2009,
+          longitude: 55.2763,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {venues.length > 0 &&
+          venues.map((x, i) => (
+            <Marker
+              key={i}
+              coordinate={{ latitude: x.lat, longitude: x.lon }}
+              title={x.name}
+              // description="Marker Description"
+            />
+          ))}
+      </MapView>
     </View>
   );
 };
 
-export default MapScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
+
+const mapStateToProps = (state) => ({
+  venues: state.venues.venues,
+  loading: state.venues.loading,
+  error: state.venues.error,
+});
+
+export default connect(mapStateToProps, { fetchVenues })(MapScreen);
